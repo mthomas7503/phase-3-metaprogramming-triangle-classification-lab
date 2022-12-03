b3da9ba371f7ca6ff502ddaf73f09d79d2b25606
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-12-03 16:44:11 -0500

    commit tree 7e6564591efac4f786d1411884cd5f5279677481

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_reader :kind
+ 
+  def initialize(side_1, side_2, side_3)
+    
+    sides = [side_1, side_2, side_3].sort
+
+    if sides.find {|side| side <= 0}
+      raise TriangleError
+    elsif sides[0] + sides[1] <= sides[2]
+      raise TriangleError
+    elsif sides.uniq.length == 1
+      @kind = :equilateral
+    elsif sides.uniq.length == 2
+      @kind = :isosceles
+    elsif sides.uniq.length == 3
+      @kind = :scalene
+    end
+  end
+
+  def kind
+    @kind
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a valid triange"
+    end
+  end
 end
